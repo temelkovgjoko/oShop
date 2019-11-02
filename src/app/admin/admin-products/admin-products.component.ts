4e108bb42b68505bb57ff3b8e6594d4737d2eb53
Gjoko <temelkovgjoko@gmail.com>
2019-11-02 18:40:09 +0100

    searching for products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,23 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: {}[];
+  filteredProducts: any[];
+  subscription: Subscription;
   constructor(
     private router: Router,
     private productService: ProductService) {
-    this.products$ = this.productService.getAll().snapshotChanges().pipe(
+    this.subscription = this.productService.getAll().snapshotChanges().pipe(
       map(changes => {
         return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
       })
-    );
+    ).subscribe(products => this.filteredProducts = this.products = products);
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p['title'].toLowerCase().includes(query.toLocaleLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit() {
